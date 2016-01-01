01ef8535faafc764068bc7d346d78bf85fe63ae9
Mitchell Wilcox <mlwilcox@gmail.com>
2015-12-31 18:47:45 -0800

    (feat) update login styles, add some animations

--- client/js/controllers/login/login.controller.js
+++ client/js/controllers/login/login.controller.js
@@ -3,9 +3,9 @@
   angular.module('app')
     .controller('LoginCtrl', LoginCtrl);
 
-  LoginCtrl.$inject = ['ApiFactory', "$state"];
+  LoginCtrl.$inject = ['ApiFactory', "$state", '$timeout'];
 
-  function LoginCtrl (ApiFactory, $state) {
+  function LoginCtrl (ApiFactory, $state, $timeout) {
     var self = this;
     self.post = function () {
       ApiFactory.post('/login', {
@@ -25,8 +25,12 @@
         self.password = '';
         if (result.error) {
           self.errorMessage = result.error;
+          self.hasError = true;
+          $timeout(function () {
+            self.hasError = false;
+          }, 2000);
           console.log('error: ', result.error);
-          $state.go('login');
+          // $state.go('login');
         } else if (result.token) {
           // result has the token and the userName and stores it in localstorage
           localStorage.setItem('token', result.token);
@@ -35,6 +39,5 @@
         }
       });
     };
-
   }
 })();
